4803d287c092b2f024c9753baaafa728535242a2
Tola-lemma <tolalemma@gmail.com>
2021-07-21 14:07:27 -0700

    modified

---
+++ java/abc.java
@@ -0,0 +1,9 @@
+
+public class abc
+{
+     public static void main(String args[]) //in String "S" is uppercase
+	 {
+		 
+	System.out.println("hello world this is first trail of java programming");//in System "S" is uppercase
+}
+}
---
+++ java/plus.java
@@ -0,0 +1,7 @@
+class plus{
+public static void main(String a[]){
+	System.out.print("This is the trail of plus sign number " + 1);
+	System.out.print("This is the trail of plus sign number " + 2);
+	
+}
+}
