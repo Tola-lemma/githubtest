efa5e3a1717acfd40aeed6012f3f97663ed5b7e8
Tola-lemma <tolalemma@gmail.com>
2021-07-21 14:09:14 -0700

    Update plus.java

--- java/plus.java
+++ java/plus.java
@@ -1,5 +1,6 @@
 class plus{
-public static void main(String a[]){
+public static void main(String a[])
+{
 	System.out.print("This is the trail of plus sign number " + 1);
 	System.out.print("This is the trail of plus sign number " + 2);
 	
